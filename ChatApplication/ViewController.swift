2e832fe8f390b92e012d8f45243ea18d2764f4f1
profile <KAWI@Kalles-MacBook-Pro.local>
2019-02-15 14:12:30 +0100

    Initial Commit

---
+++ ChatApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApplication
+//
+//  Created by Kalle Teglstrup Wilsen on 15/02/2019.
+//  Copyright © 2019 Kalle Teglstrup Wilsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
