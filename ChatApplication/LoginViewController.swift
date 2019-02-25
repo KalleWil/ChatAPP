c139d0a8ef2ec1eaeac923d24c7cd5549b95b2cd
KalleWil <kalle-wil@hotmail.com>
2019-02-25 16:30:12 +0100

    Correction of login story and main story

---
+++ ChatApplication/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  ChatApplication
+//
+//  Created by Kalle Teglstrup Wilsen on 25/02/2019.
+//  Copyright © 2019 Kalle Teglstrup Wilsen. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBAction func loginIn(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
