3de5810fa73f93470d34371c77f0246aad2f4975
KalleWil <kalle-wil@hotmail.com>
2019-04-02 08:43:42 +0200

    Pop-up message to indicate whether user has been created or not. Email check removed due to bug with retrieving data from database.

--- RegisterViewController.swift
+++ RegisterViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseDatabase
+import Firebase
 
 
 
@@ -34,26 +35,87 @@ class RegisterViewController: UIViewController {
         
     }
     
-    func emailCheck() {
-        databaseHandle = ref.child("users").observe(.value) { (snapshot) in
+    func emailCheck(){
+        
+        ref.child("users").observeSingleEvent(of: .value, with: { (snapshot) in
+            if !snapshot.exists(){
             for childSnap in snapshot.children.allObjects{
                 let snap = childSnap as! DataSnapshot
-                self.databaseHandle = self.ref.child("users/" + snap.key + "/email/").observe(.value, with: { (emailSnapshot) in
+                self.ref.child("users/" + snap.key + "/email/").observeSingleEvent(of: .value, with: { (emailSnapshot) in
+                    
+                    print("Test stop 1")
                     let emailCheck = emailSnapshot.value as? String
                     if let actualEmail = emailCheck{
-                        if actualEmail == self.email{
-                            self.emailExists = true
-                            print("Email exists in database")
-                        }
+
+                            if actualEmail == self.email{
+                                self.emailExists = true
+                            }
+                            else if actualEmail != self.email && self.emailExists == false{
+                                self.emailExists = false
+                            }
+                        
+                        print("actualEmail: " + actualEmail)
+                    }
+                    else{
+                        print("Test stop 2")
+                        self.emailExists = false
                     }
                 })
-                
             }
+            }
+            else{
+                print("Error no snapshot")
+            }
+        })
+        
+        
+        if self.emailExists == false{
+            self.createUser()
         }
-        if emailExists == true{}
+        else if self.emailExists == true{
+            print("Avoid creating user")
+        }
+    }
+    
+    func createUser() {
+        if password == confirmPassword{
+            if firstname != "" || lastname != "" || email != "" || password != "" || confirmPassword != "" {
+                
+                let childID = self.ref
+                    .child("users")
+                    .childByAutoId()
+                let childIDkey = childID.key!
+                
+                ref.child("users/" + childIDkey + "/firstname").setValue(firstname)
+                ref.child("users/" + childIDkey + "/lastname").setValue(lastname)
+                ref.child("users/" + childIDkey + "/email").setValue(email)
+                ref.child("users/" + childIDkey + "/password").setValue(password)
+                
+                
+                let userCreateSucces = UIAlertController(title: "ChatAPP", message: "User created!", preferredStyle: UIAlertController.Style.alert)
+                userCreateSucces.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default){
+                    UIAlertAction in
+                    self.performSegue(withIdentifier: "registerSegue", sender: self)
+                })
+                self.present(userCreateSucces, animated: true, completion: nil)
+                
+                print("User created succesfully!")
+            }
+        
         else{
-            emailExists = false
+            let userCreateFailMissing = UIAlertController(title: "ChatAPP", message: "User could not be created - Some fields are left empty", preferredStyle: UIAlertController.Style.alert)
+            userCreateFailMissing.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.cancel))
+            self.present(userCreateFailMissing, animated: true, completion: nil)
         }
+            
+        }
+        else{
+            let userCreateFailPassword = UIAlertController(title: "ChatAPP", message: "User could not be created - Passwords dont match", preferredStyle: UIAlertController.Style.alert)
+            userCreateFailPassword.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.cancel))
+            self.present(userCreateFailPassword, animated: true, completion: nil)
+
+        }
+    
     }
     
     @IBAction func registerUser(_ sender: UIButton) {
@@ -70,31 +132,9 @@ class RegisterViewController: UIViewController {
         print("Email: " + email)
         print("Password: " + password)
         print("ConfirmPassword: " + confirmPassword)
+
+        createUser()
         
-        emailCheck()
-        
-        if password == confirmPassword{
-            if emailExists == !true{
-            
-            let childID = self.ref
-                .child("users")
-                .childByAutoId()
-            let childIDkey = childID.key!
-            
-            ref.child("users/" + childIDkey + "/firstname").setValue(firstname)
-            ref.child("users/" + childIDkey + "/lastname").setValue(lastname)
-            ref.child("users/" + childIDkey + "/email").setValue(email)
-            ref.child("users/" + childIDkey + "/password").setValue(password)
-            
-            print("User created succesfully!")
-            }
-            else{
-                print("ERROR! - Email already exists")
-            }
-        }
-        else{
-            print("ERROR! - Passwords dont match")
-        }
         
  
     }
